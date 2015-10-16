17dfd21cce444836be6dbbef610fe318ee0461da
arcticmatt <mlimcollege@gmail.com>
2015-10-16 14:05:02 -0700

    Finish up by adding some tests

--- KPCB/hashmap/hashmap.cpp
+++ KPCB/hashmap/hashmap.cpp
@@ -1,7 +1,9 @@
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <cassert>
 #include <iostream>
+#include <type_traits>
 #include "hashmap.h"
 
 using namespace std;
@@ -14,25 +16,101 @@ int main(int argc, const char* argv[]) {
     else
         size = DEFAULT_SIZE;
 
-    clock_t begin_time = clock();
-
+    vector<string> keys;
+    vector<string> values;
     Hashmap<string> map(size);
-    fill(map, size);
-    cout << "Hashmap load is " << map.load() << endl;
-
-    cout << "Fill time for " << size << " elements = " <<
+    clock_t begin_time = clock();
+    test_map(map, size / 2, size / 3, size);
+    test_change_value(map);
+    cout << "Test takes " <<
         float(clock() - begin_time) / CLOCKS_PER_SEC << " seconds" << endl;
 }
 
 /*** Test Functions ***/
 /*
- * Fills the hashmap with random key/value pairs.
+ * Tests hashmap by filling it with a bunch of values. Then calls the
+ * delete method on a fraction of the keys, and makes sure the correct number
+ * get deleted by comparing the fraction deleted to the final load value.
+ *
+ * For this test to work, all inserted keys need to be UNIQUE so that fill
+ * always inserts new key/value pairs.
  */
-void fill(Hashmap<string>& map, int num_items) {
+void test_map(Hashmap<string>& map, int num_fill, int num_delete, int map_size) {
+    cout << "Testing hashmap..." << endl;
+    vector<string> fill_keys;
+    vector<string> fill_vals;
+    fill(map, num_fill, fill_keys, fill_vals);
+    int num_keys = fill_keys.size();
+    cout << "Inserted " << num_keys << " key/value pairs" << endl;
+
+    num_delete = num_keys < num_delete ? num_keys : num_delete;
+    vector<string> delete_keys;
+    vector<string> delete_vals;
+    for (int i = 0; i < num_delete; i++)
+        delete_keys.push_back(fill_keys[i]);
+    delete_values(map, num_delete, delete_keys, delete_vals);
+    // Should be able to delete every key, because when filling, we only
+    // inserted the key/value pairs that were successful into our vectors
+    cout << "Deleted " << delete_keys.size() << " key/value pairs" << endl;
+    assert (delete_keys.size() == num_delete);
+
+    // Make sure the values we deleted match up with the values we inserted
+    for (int i = 0; i < num_delete; i++) {
+        //cout << fill_vals[i] << ", " << delete_vals[i] << endl;
+        assert (fill_vals[i].compare(delete_vals[i]) == 0);
+    }
+
+    // Make sure the load value makes sense
+    float practical_load = (float) (num_fill - num_delete) / map_size;
+    float map_load = map.load();
+    cout << "Practical load = " << practical_load << endl;
+    cout << "Map load = " << map_load << endl;
+    assert (abs(practical_load - map_load) < EPSILON);
+}
+
+/*
+ * A simple test that makes sure changing the value for a key works.
+ */
+void test_change_value(Hashmap<string>& map) {
+    string key = "matt";
+    string val = "sucks";
+    string val2 = "rocks";
+    map.set(key, val);
+    string orig = map.get(key);
+    map.set(key, val2);
+    string changed = map.get(key);
+
+    assert (orig.compare("sucks") == 0);
+    assert (orig.compare(changed) != 0);
+    assert (changed.compare("rocks") == 0);
+}
+
+/*
+ * Fills the hashmap with random key/value pairs. Puts the key/value pairs
+ * that were successfully inserted into the hashmap into the passed-in vectors.
+ */
+void fill(Hashmap<string>& map, int num_items, vector<string>& keys,
+        vector<string>& values) {
     for (int i = 0; i < num_items; i++) {
-        string key = random_string(7);
-        string value = random_string(7);
-        map.set(key, value);
+        string key = random_string(10);
+        string value = random_string(10);
+        if (map.set(key, value)) {
+            keys.push_back(key);
+            values.push_back(value);
+        }
+    }
+}
+
+/*
+ * Deletes a certain number of keys from the hashmap. Puts the values that
+ * were deleted into the passed-in vector.
+ */
+void delete_values(Hashmap<string>&map, int num_items, vector<string>& keys,
+        vector<string>& values) {
+    for (int i = 0; i < num_items; i++) {
+        string key_to_del = keys[i];
+        string del_value = map.delete_value(key_to_del);
+        values.push_back(del_value);
     }
 }
 
@@ -102,16 +180,26 @@ unsigned long Hashmap<T>::hash2(string key) {
     return sum % size;
 }
 
+/* This method searches the hashmap for an index to insert the passed-in key.
+ * If the passed-in key already exists in the table, it returns that index
+ * so that the key can be updated with the new value.
+ *
+ * We search for a vacant index using double hashing, which is similar to linear
+ * probing. Basically, we search for an index by starting at the hash index of
+ * our key, and then taking steps whose sizes are based on a different hash
+ * function (hash2, in our case).
+ */
 template<typename T>
-unsigned long Hashmap<T>::get_index(string key) {
+unsigned long Hashmap<T>::get_insert_index(string key) {
     unsigned long index = hash(key);
     unsigned long num_steps = 0;
     unsigned long incr = hash2(key);
     bool same_key = false;
     node<T> *curr_node = slot[index];
+
     // Search for vacant space, or for space with passed-in key
     while (curr_node != NULL) {
-        if (num_steps == size)
+        if (num_steps == size) // have looped
             break;
         if (key.compare(curr_node->key) == 0) {
             same_key = true;
@@ -129,6 +217,36 @@ unsigned long Hashmap<T>::get_index(string key) {
 }
 
 /*
+ * This method searches the hashmap for the index of the passed-in key.
+ * This index can be used to delete a key, or reference the value of a key.
+ *
+ * We search for keys using double hashing, which is similar to linear probing.
+ * Basically, we search for an index by starting at the hash index of our key,
+ * and then taking steps whose sizes are based on a different hash function (hash2,
+ * in our case).
+ */
+template<typename T>
+unsigned long Hashmap<T>::get_index(string key) {
+    unsigned long index = hash(key);
+    unsigned long num_steps = 0;
+    unsigned long incr = hash2(key);
+    node<T> *curr_node = slot[index];
+
+    // Search for the key
+    while (true) {
+        if (num_steps == size) // have looped
+            break;
+        if (curr_node != NULL && key.compare(curr_node->key) == 0)
+            return index;
+        index = (index + incr) % size;
+        curr_node = slot[index];
+        num_steps++;
+    }
+
+    return -1;
+}
+
+/*
  * Stores the given key/value pair in the hashmap. Returns a boolean value
  * indicating success/failure of the operation.
  *
@@ -138,7 +256,7 @@ unsigned long Hashmap<T>::get_index(string key) {
  */
 template<typename T>
 bool Hashmap<T>::set(string key, T value) {
-    unsigned long index = get_index(key);
+    unsigned long index = get_insert_index(key);
     if (index == -1) {
         return false;
     } else if (slot[index] != NULL) { // Just change value
@@ -147,6 +265,7 @@ bool Hashmap<T>::set(string key, T value) {
     } else {
         slot[index] = new node<T>(key, value);
     }
+    return true;
 }
 
 /*
@@ -155,22 +274,28 @@ bool Hashmap<T>::set(string key, T value) {
 template<typename T>
 T Hashmap<T>::get(string key) {
     unsigned long index = get_index(key);
-    if (index == -1)
-        return NULL;
-    else
-        return slot[index];
-
+    if (index == -1) {
+        // NULL is not a compatible return type with every type in C++. So
+        // we'll just initialize a default object of type T and return that.
+        T obj = T();
+        return obj;
+    } else {
+        return slot[index]->value;
+    }
 }
 
 /*
  * Deletes the value associated with the given key, returning the value on
- * success or null if the key has no value.
+ * success or null if the key has no value or if the key doesn't exist.
  */
 template<typename T>
 T Hashmap<T>::delete_value(string key) {
     unsigned long index = get_index(key);
     if (index == -1) {
-        return NULL;
+        // NULL is not a compatible return type with every type in C++. So
+        // we'll just initialize a default object of type T and return that.
+        T obj = T();
+        return obj;
     } else {
         // Delete node from array and memory, and return value
         T value = slot[index]->value;
--- KPCB/hashmap/hashmap.h
+++ KPCB/hashmap/hashmap.h
@@ -1,13 +1,17 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
+#include <vector>
 #include <stdio.h>
 #include <string>
 
 using namespace std;
 
-const int DEFAULT_SIZE = 10000;
-const string CHARSET = "abcdefghijklmnopqrstuvwxyz";
+const float EPSILON = .1;
+const string STRING_NULL = string();
+const int INT_NULL = -1;
+const int DEFAULT_SIZE = 1000000;
+const string CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 /* Nodes hold key/value pairs */
 template<typename T>
@@ -29,6 +33,7 @@ class Hashmap
         ~Hashmap();
         unsigned long hash(string key);
         unsigned long hash2(string key);
+        unsigned long get_insert_index(string key);
         unsigned long get_index(string key);
         bool set(string key, T value);
         T get(string key);
@@ -40,7 +45,14 @@ class Hashmap
         unsigned long size;
 };
 
-void fill(Hashmap<string>& map, int num_items);
+/* Test functions */
+void test_map(Hashmap<string>& map, int num_fill, int num_delete, int map_size);
+void test_change_value(Hashmap<string>& map);
+void fill(Hashmap<string>& map, int num_items, vector<string>& keys,
+        vector<string>& values);
+void delete_values(Hashmap<string>&map, int num_items, vector<string>& keys,
+        vector<string>& values);
+;
 string random_string(int length);
 
 #endif
